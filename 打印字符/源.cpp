8fd447dad162494376895210f6eb0c2f3f161a89
Dengzhibin <117286854+bingbang1103@users.noreply.github.com>
2023-02-05 19:22:18 +0800

    Add files via upload

---
+++ 打印字符/源.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',0 };
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+return 0;
+}
