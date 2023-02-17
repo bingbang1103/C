4e54d8cf03c5f2cbb3e7bdc6965259cd471670d0
Dengzhibin <117286854+bingbang1103@users.noreply.github.com>
2023-02-17 21:57:07 +0800

    Add files via upload

---
+++ 试除法（找素数）/源.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (i == j)
+		{
+			printf("%d ",i);
+			count++;
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}	
