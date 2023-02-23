a0beb782cdda9e882e2e2033cc90791c4a3e634b
Dengzhibin <117286854+bingbang1103@users.noreply.github.com>
2023-02-23 22:49:32 +0800

    Add files via upload

---
+++ 课程2/源.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	printf("输入一行字符：\n");
+
+	char ch;
+
+	int i, count = 0, word = 0;
+
+	while ((ch = getchar()) != '\n')
+
+		if (ch == ' ')
+
+			word = 0;
+
+		else if (word == 0)
+
+		{
+			word = 1;
+
+			count++;
+
+		}
+
+	printf("总共有 %d 个单词\n", count);
+
+	return 0;
+
+}
